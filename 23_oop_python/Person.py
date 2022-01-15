f64292d88afead649d7aa854dc849e8183e15fbf
denimcouch <alexmata92@ymail.com>
2022-01-15 08:27:41 -0600

    added notes

--- 23_oop_python/Person.py
+++ 23_oop_python/Person.py
@@ -4,11 +4,13 @@ class Person:
     self.age = age
     self.hobbies = hobbies
 
-
+  # The __str__ method is used to display useful info
+  # about an object in string format
   def __str__(self):
     return f"Person {self.name}, {self.age} years old."
 
-  
+  # The __repr__ method is mean to display a info
+  # about an object that could be useful for devs
   def __repr__(self):
     return f"<Person Object('{self.name}', {self.age}, {self.hobbies})>"
 
